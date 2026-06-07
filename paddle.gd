189efdf2dd369601235fc93dd52de5355eafb459
Ryan Safarzadeh <safarzadehryan@gmail.com>
2026-06-07 12:14:20 -0700

    init (paddles)

---
+++ paddle.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED: float = 350.0
+
+@export var player_id: int
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed('up-%d' % player_id):
+		velocity = Vector2(0, -SPEED)
+	if event.is_action_pressed('down-%d' % player_id):
+		velocity = Vector2(0, SPEED)
+
+	if event.is_action_released('up-%d' % player_id) or \
+		event.is_action_released('down-%d' % player_id):
+			velocity = Vector2.ZERO
+		
+func _physics_process(delta: float) -> void:
+	move_and_slide()
